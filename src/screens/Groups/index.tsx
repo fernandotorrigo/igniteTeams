b24060a77f22335e6a5ef1a1e39676fc0daaad4d
Fernando Torrigo <fernandotorrigo@ext.cvccorp.com.br>
2022-10-06 12:01:54 -0300

    setup initial

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Groups = () => {
+  return (
+    <View>
+      <Text>Groups</Text>
+    </View>
+  );
+};
+
+export default Groups;
