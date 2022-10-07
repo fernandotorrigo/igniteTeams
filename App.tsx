99916797b879e0540276056bd86e6745ec45b2aa
Fernando Torrigo <fernandotorrigo@ext.cvccorp.com.br>
2022-10-07 12:47:03 -0300

    implementing loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import Loading from "@components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import Groups from "@screens/Groups";
+import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
